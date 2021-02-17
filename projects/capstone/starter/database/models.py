b8a738896cb61ade3f18f750c030361573f280c8
Njoud Sultan <njoudsultan@gmail.com>
2021-02-17 12:09:21 +0300

    code update

--- projects/capstone/starter/database/models.py
+++ projects/capstone/starter/database/models.py
@@ -1,22 +1,37 @@
 import os
-from sqlalchemy import Column, String, Integer, create_engine, Date
+from sqlalchemy import Column, String, Integer, Date
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_name = "casting_agency"
-database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+database_filename = "database.db"
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_path = "sqlite:///{}".format(os.path.join(project_dir, database_filename))
 
 db = SQLAlchemy()
 
-
-def setup_db(app, database_path=database_path):
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+def setup_db(app):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
+
+'''
+db_drop_and_create_all()
+    drops the database tables and starts fresh
+    can be used to initialize a clean database
+    !!NOTE you can change the database_filename variable to have multiple verisons of a database
+'''
+def db_drop_and_create_all():
+    db.drop_all()
     db.create_all()
 
-
+'''
+Movie and Actor classes 
+'''
 class Movie(db.Model):
     __tablename__ = 'movies'
 
